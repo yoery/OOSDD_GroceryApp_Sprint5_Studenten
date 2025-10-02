767f24ecd94bc4c63d517a88e0e7ff7a11befe7d
Eugène van Roden <ecrvanroden@hotmail.com>
2025-10-02 11:45:35 +0200

    Testproject toegevoegd

---
+++ TestCore/TestHelpers.cs
@@ -0,0 +1,46 @@
+using Grocery.Core.Helpers;
+
+namespace TestCore
+{
+    public class TestHelpers
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+
+        //Happy flow
+        [Test]
+        public void TestPasswordHelperReturnsTrue()
+        {
+            string password = "user3";
+            string passwordHash = "sxnIcZdYt8wC8MYWcQVQjQ==.FKd5Z/jwxPv3a63lX+uvQ0+P7EuNYZybvkmdhbnkIHA=";
+            Assert.IsTrue(PasswordHelper.VerifyPassword(password, passwordHash));
+        }
+
+        [TestCase("user1", "IunRhDKa+fWo8+4/Qfj7Pg==.kDxZnUQHCZun6gLIE6d9oeULLRIuRmxmH2QKJv2IM08=")]
+        [TestCase("user3", "sxnIcZdYt8wC8MYWcQVQjQ==.FKd5Z/jwxPv3a63lX+uvQ0+P7EuNYZybvkmdhbnkIHA=")]
+        public void TestPasswordHelperReturnsTrue(string password, string passwordHash)
+        {
+            Assert.IsTrue(PasswordHelper.VerifyPassword(password, passwordHash));
+        }
+
+
+        //Unhappy flow
+        [Test]
+        public void TestPasswordHelperReturnsFalse()
+        {
+            string password = "user3";
+            string passwordHash = "sxnIcZdYt8wC8MYWcQVQjQ";
+            Assert.IsFalse(PasswordHelper.VerifyPassword(password, passwordHash));
+        }
+
+        [TestCase("user1", "IunRhDKa+fWo8+4/Qfj7Pg")]
+        [TestCase("user3", "sxnIcZdYt8wC8MYWcQVQjQ")]
+        public void TestPasswordHelperReturnsFalse(string password, string passwordHash)
+        {
+            Assert.IsFalse(PasswordHelper.VerifyPassword(password, passwordHash));
+        }
+    }
+}
