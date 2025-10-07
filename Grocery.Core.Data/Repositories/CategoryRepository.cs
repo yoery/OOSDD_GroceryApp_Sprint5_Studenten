457d113cc69d7c0060b4ab5f69ffed7c6e2575ca
yoery <81920433+yoery@users.noreply.github.com>
2025-10-07 20:35:36 +0200

    liet nog geen categorieen zien dus heb die toegevoegd.

--- Grocery.Core.Data/Repositories/CategoryRepository.cs
+++ Grocery.Core.Data/Repositories/CategoryRepository.cs
@@ -9,6 +9,20 @@ namespace Grocery.Core.Data.Repositories
     {
         private readonly List<Category> _categories = new();
 
+        public CategoryRepository()
+        {
+            _categories.Add(new Category { Id = 1, Name = "Fruit", Description = "Vers fruit" });
+            _categories.Add(new Category { Id = 2, Name = "Groente", Description = "Verse groente" });
+            _categories.Add(new Category { Id = 3, Name = "Zuivel", Description = "Melk, kaas, yoghurt" });
+            _categories.Add(new Category { Id = 4, Name = "Vlees", Description = "Vers vlees en gevogelte" });
+            _categories.Add(new Category { Id = 5, Name = "Brood", Description = "Brood en bakkerijproducten" });
+            _categories.Add(new Category { Id = 6, Name = "Dranken", Description = "Frisdrank, sap, water" });
+            _categories.Add(new Category { Id = 7, Name = "Snacks", Description = "Chips, koekjes, snoep" });
+            _categories.Add(new Category { Id = 8, Name = "Diepvries", Description = "Diepvriesproducten" });
+            _categories.Add(new Category { Id = 9, Name = "Kruiden", Description = "Specerijen en kruiden" });
+            _categories.Add(new Category { Id = 10, Name = "Non-food", Description = "Huishoudelijke artikelen" });
+        }
+
         public IEnumerable<Category> GetAll() => _categories;
 
         public Category? GetById(int id) => _categories.FirstOrDefault(c => c.Id == id);
